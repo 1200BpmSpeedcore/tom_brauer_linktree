6438aaa9d0cf73a76d1bb5e25efeb4e3d05f4f6b
Tom Brauer <tombrauer0102@gmail.com>
2025-11-11 23:50:21 +0100

    Add navbar
    * add navbar
    * add home component and link
    * add portfolio component and link
    * add external links (xing, linkedin, github, email)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomePage } from './forms/home-page/home-page';
+import { Portfolio } from './forms/portfolio/portfolio';
+
+
+export const routes: Routes = [
+  {path: '', component: HomePage},
+  {path: 'home', redirectTo: '', pathMatch: 'full'},
+  {path: 'portfolio', component: Portfolio}
+];
