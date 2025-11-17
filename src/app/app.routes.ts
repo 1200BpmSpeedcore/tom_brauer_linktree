10e178376b1557c2b70121683b94180230ad5419
Tom Brauer <tombrauer0102@gmail.com>
2025-11-17 12:21:48 +0100

    Add imprint
    * add imprint component with sparse design
    * add link in footer
    * add ai training and web scraping note to footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,18 @@ import { Routes } from '@angular/router';
 
 import { HomePage } from './forms/home-page/home-page';
 import { Portfolio } from './forms/portfolio/portfolio';
+import { Disclosure } from './forms/imprint/disclosure/disclosure';
 
 
 export const routes: Routes = [
+  // Header
   {path: '', component: HomePage},
   {path: 'home', redirectTo: '', pathMatch: 'full'},
-  {path: 'portfolio', component: Portfolio}
+  {path: 'portfolio', component: Portfolio},
+
+  // Imprint
+  {path: 'imprint', component: Disclosure},
+  {path: 'legal', redirectTo: 'imprint', pathMatch: 'full'},
+  {path: 'legal-disclosure', redirectTo: 'imprint', pathMatch: 'full'},
+  {path: 'disclosure', redirectTo: 'imprint', pathMatch: 'full'},
 ];
