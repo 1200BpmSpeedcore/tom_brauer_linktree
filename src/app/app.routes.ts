be5a7709e2fa4a4ddfd2722cf143a17e80a7e5fb
Tom Brauer <tombrauer0102@gmail.com>
2025-11-17 20:25:27 +0100

    Add Datenschutzerklaerung
    * add Datenschutzerklaerung component with sparse design
    * add link in footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { Disclosure } from './forms/imprint/disclosure/disclosure';
 import { Impressum } from './forms/imprint/impressum/impressum';
 
 import { PrivacyPolicy } from './forms/privacy/privacy-policy/privacy-policy';
+import { Datenschutzerklaerung } from './forms/privacy/datenschutzerklaerung/datenschutzerklaerung';
 
 
 export const routes: Routes = [
@@ -27,5 +28,9 @@ export const routes: Routes = [
   // Privacy Policy
   {path: 'privacy', component: PrivacyPolicy},
   {path: 'privacy-policy', redirectTo: 'privacy', pathMatch: 'full'},
-  {path: 'data-protection', redirectTo: 'privacy', pathMatch: 'full'}
+  {path: 'data-protection', redirectTo: 'privacy', pathMatch: 'full'},
+
+  // Datenschutz
+  {path: 'datenschutz', component: Datenschutzerklaerung},
+  {path: 'datenschutzerklaerung', redirectTo: 'datenschutz', pathMatch: 'full'}
 ];
