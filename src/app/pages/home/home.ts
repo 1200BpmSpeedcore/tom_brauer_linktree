91f43450cc8caaf391f8eb408a410ae9108feb05
Tom Brauer <tombrauer0102@gmail.com>
2026-01-12 17:45:27 +0100

    Add hero section to home page
    * add hero section with picture of me and call-to-action button

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { InternalNavButton } from '../../models';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [
+    RouterModule,
+    CommonModule
+  ],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class HomePage {
-
+  readonly buttonConfigs: InternalNavButton[] = [
+    {
+      label: 'My Projects',
+      tooltip: 'Go to my portfolio page',
+      variant: 'outline-primary',
+      size: 'lg',
+      disabled: false,
+      routerLink: '/portfolio',
+      exact: true
+    },
+    {
+      label: 'About Me',
+      tooltip: 'Go to my \"About Me\" page',
+      variant: 'outline-secondary',
+      size: 'lg',
+      disabled: false,
+      // TODO: Add about me routing
+      routerLink: '/',
+      exact: true,
+    }
+  ];
 }
