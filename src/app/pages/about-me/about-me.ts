bcafe607eb510f7df300c4068ee20b15ccd060d1
Tom Brauer <tombrauer0102@gmail.com>
2026-01-27 06:18:56 +0100

    Add introductory content to About Me page

--- src/app/pages/about-me/about-me.ts
+++ src/app/pages/about-me/about-me.ts
@@ -2,10 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
+  standalone: true,
   imports: [],
   templateUrl: './about-me.html',
   styleUrl: './about-me.scss',
 })
 export class AboutMe {
-
+  readonly dateToday: Date = new Date();
+  readonly birthDate: Date = new Date('2002-02-01');
+  readonly currentAge = (): number => {
+    return this.dateToday.getFullYear() - this.birthDate.getFullYear()
+      - ((this.dateToday.getMonth() < this.birthDate.getMonth()
+        || (
+          this.dateToday.getMonth() === this.birthDate.getMonth()
+          && this.dateToday.getDate() < this.birthDate.getDate()
+        )
+      ) ? 1 : 0);
+  }
 }
